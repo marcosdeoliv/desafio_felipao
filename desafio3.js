6730db5a46039f17ee4ca7c7c74cc68d4c744186
marcosdeoliv <marcosdeoliveiramonteiro@gmail.com>
2024-05-24 03:23:42 -0400

    desafio Felipão finalizadogit add .!

--- desafio3.js
+++ desafio3.js
@@ -6,16 +6,18 @@ function calcularRank(vitorias, derrotas) {
     let nivel;
     if (saldoVitorias <= 10) {
         nivel = 'Ferro';
-    } else if (saldoVitorias <= 20) {
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
         nivel = 'Bronze';
-    } else if (saldoVitorias <= 50) {
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
         nivel = 'Prata';
-    } else if (saldoVitorias <= 80) {
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
         nivel = 'Ouro';
-    } else if (saldoVitorias <= 90) {
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
         nivel = 'Diamante';
-    } else if (saldoVitorias <= 100) {
-        nivel = 'Lendário e Imortal';
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = 'Lendário';
+    } else if(saldoVitorias > 101){
+        nivel = 'Imortal';
     }
 
     // Retornar a mensagem formatada
